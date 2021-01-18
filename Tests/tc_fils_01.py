421ceb675dc28f2bd907ecb2273759a06e1a884f
kietalang99 <kietalang.dev@gmail.com>
2021-01-18 09:58:35 +0700

    Filter by status Assigned completed

--- Tests/tc_fils_01.py
+++ Tests/tc_fils_01.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import unittest
 import time
-
 from Pages.loginPage import LoginPage
 
 class loginTest(unittest.TestCase):
@@ -9,16 +8,18 @@ class loginTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome(executable_path="C:/Users/Admin/PycharmProjects/admin/Drivers/chromedriver.exe")
-        cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
-
-    def test_login_valid(self):
-        driver = self.driver
+        driver = cls.driver
         driver.get("https://admin-helper-f21c1.web.app/login")
         login = LoginPage(driver)
         login.enter_email("admin@gmail.com")
         login.enter_password("123456")
         login.click_login()
+        cls.driver.implicitly_wait(10)
+
+    def test_filter_device(self):
+        self.driver.find_element_by_xpath('//*[@id="sidebar"]/ul/li[2]/a').click()
+        self.driver.find_element_by_xpath("//select[@id='device-status']/option[@value='ASSIGNED']").click()
         time.sleep(2)
 
     @classmethod
