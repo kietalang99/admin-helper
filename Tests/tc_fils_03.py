80e303e7a01b54e601e1ac7f475834bdbeed2712
kietalang99 <kietalang.dev@gmail.com>
2021-01-18 10:14:19 +0700

    Test filter by Discarded status is completed

---
+++ Tests/tc_fils_03.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import unittest
+import time
+from Pages.loginPage import LoginPage
+
+class filterByStatus(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="C:/Users/Admin/PycharmProjects/admin/Drivers/chromedriver.exe")
+        cls.driver.maximize_window()
+        driver = cls.driver
+        driver.get("https://admin-helper-f21c1.web.app/login")
+        login = LoginPage(driver)
+        login.enter_email("admin@gmail.com")
+        login.enter_password("123456")
+        login.click_login()
+        cls.driver.implicitly_wait(10)
+
+    def test_filter_status_02(self):
+        self.driver.find_element_by_xpath('//*[@id="sidebar"]/ul/li[2]/a').click()
+        self.driver.find_element_by_xpath("//select[@id='device-status']/option[text()=' Discarded ']").click()
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test completed")
