0d8475fa2bcee3b2854f64572e3a087b1f22c018
kietalang99 <kietalang.dev@gmail.com>
2021-01-18 14:07:06 +0700

    changed name of test

--- Tests/tc_fildc_02.py
+++ Tests/tc_fildc_02.py
@@ -17,7 +17,7 @@ class filterByDeviceCategory(unittest.TestCase):
         login.click_login()
         cls.driver.implicitly_wait(10)
 
-    def test_filter_device_category_01(self):
+    def test_filter_device_category_02(self):
         self.driver.find_element_by_xpath('//*[@id="sidebar"]/ul/li[2]/a').click()
         self.driver.find_element_by_xpath("//select[@id='device-category']/option[text()=' Laptop ']").click()
         time.sleep(2)
